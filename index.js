8d99e49482290bda0063d977c81ca590f97f8569
philip dutra <monsterphilip10@gmail.com>
2023-04-10 22:04:18 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name1, email1){
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+      body: JSON.stringify({name: `${name1}`, email: `${email1}`}) 
+
+    })
+    .then(resp => resp.json())
+    .then(obj => document.querySelector("body").append(obj.id))
+
+    .catch(function (error){ 
+        alert("Somethings Not Right"); 
+        document.querySelector("body").append(error.message)})
+}
+
